e3f4ad9706c6a9e83f885e5a3a86e0d407379d82
zhy <2063218120@qq.com>
2024-07-29 10:27:40 +0800

    fix: different platform log error
    fix the different platform log error
    
    log: fix the different platform log error
    detail: fix the different platform log error

--- muparser_engine/OperaionByMuParser.cpp
+++ muparser_engine/OperaionByMuParser.cpp
@@ -12,17 +12,22 @@ double OperaionByMuParser::ComputeExpressionValue(QString str)
         double result = 0;
 
         mu::Parser m_parser;
-
+#if defined(_UNICODE)
+        m_parser.SetExpr(str.toStdWString());
+#else
         m_parser.SetExpr(str.toStdString());
-
+#endif
         return m_parser.Eval();
     }
 
     catch (mu::Parser::exception_type &e)
     {
         //上报错误
+#if defined(_UNICODE)
+        std::cout << QString::fromStdWString(e.GetMsg()).toStdString() << std::endl;
+#else
         std::cout << QString::fromStdString(e.GetMsg()).toStdString() << std::endl;
-
+#endif
         //返回值
         return Q_QNAN;
     }
